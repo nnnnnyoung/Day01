944155d5d58f7626553146ff9093f749c2480c69
nnnnnyoung <tjs40106@naver.com>
2022-08-30 18:47:31 +0900

    Day01_02

---
+++ src/Day01/Day01_02.java
@@ -0,0 +1,19 @@
+package Day01;
+
+public class Day01_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int a = 1234; 가 있다. < 손코딩한 후  실제코드로 피드백하세요>
+		   백단위만 출력하시오. */
+
+				int a =1234;
+				a = ((a%1000)/100);
+				
+				
+				System.out.println(a);
+
+	}
+
+}
