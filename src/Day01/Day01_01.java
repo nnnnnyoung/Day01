b6354411d99ec77137ac413f4966cd1296ee66a5
nnnnnyoung <tjs40106@naver.com>
2022-08-30 18:44:06 +0900

    commit

---
+++ src/Day01/Day01_01.java
@@ -0,0 +1,18 @@
+package Day01;
+
+public class Day01_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*철수와 철희는 홀짝게임을 한다. < 손코딩한 후  실제코드로 피드백하세요>
+		   철수가 제시한 숫자가 홀인찌 짝인지 구분하려고 한다.
+		   철수가 제시한 숫자를 int a = 32; 라고 가정한다.
+		   int result에는 홀과 짝을 판별한 결과가 저장된다. 홀이면 1이 저장되고 짝이면 0이 저장된다.*/
+
+				int a = 32;
+				int result = a%2;
+				
+				System.out.println(result);
+	}
+
+}
